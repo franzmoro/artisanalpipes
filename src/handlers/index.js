5361ac6c51b20ae69965945b423ebda03f545e3e
franzmoro <franzmoro88@gmail.com>
2016-12-02 12:06:54 +0100

    :art: refactor views handlers

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,6 +1,3 @@
 module.exports = {
-  about: require('./about.js'),
-  checkout: require('./checkout.js'),
-  contact: require('./contact.js'),
-  pipes: require('./pipes.js')
+  views: require('./views.js')
 };
