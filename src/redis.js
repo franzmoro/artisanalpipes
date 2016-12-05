bbfb8a8f512a523ccd27ae895bf1c2a63aba8b88
franzmoro <franzmoro88@gmail.com>
2016-12-05 20:19:59 +0100

    config: redis url
    relates to #2

--- src/redis.js
+++ src/redis.js
@@ -2,16 +2,22 @@ require('env2')('./.env');
 const redis = require('redis');
 
 module.exports = config => {
-  const port = config.REDIS_PORT || 6379;
-  const host = config.REDIS_HOST || 'localhost';
-  const db = config.REDIS_DB || 0;
 
-  const client = redis.createClient(port, host);
-
-  client.select(db);
+  let client;
+  if (config.REDIS_URL) {
+    client = redis.createClient(config.REDIS_URL);
+    client._connectionURL = config.REDIS_URL;
+  } else {
+    const port = config.REDIS_PORT || 6379;
+    const host = config.REDIS_HOST || 'localhost';
+    const db = config.REDIS_DB || 0;
+    client = redis.createClient(port, host);
+    client.select(db);
+    client._connectionURL = `redis://${host}:${port}/${db}`;
+  }
 
   client.on('connect', () => {
-    console.log(`connected to redis: ${host}:${port}`);
+    console.log(`connected to redis: ${client._connectionURL}`);
   });
 
   client.on('error', err => {
